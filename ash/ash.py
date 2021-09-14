32e17ae2f71b087ea101ab3de0381e192eb478df
Alexander Dittmann <dittmann@astro.umd.edu>
2021-09-14 13:18:44 -0400

    2D update and example

--- ash/ash.py
+++ ash/ash.py
@@ -39,16 +39,18 @@ def ash2d(xvals, yvals, nbins=20, nshifts=10, weights=None, padVals=False):
 
   nsg = (nbins+2)*nshifts
   xgrid = np.linspace(xmin, xmax, (nbins+2)*nshifts)
-  ygrid = np.linspace(xmin, xmax, (nbins+2)*nshifts)
+  ygrid = np.linspace(ymin, ymax, (nbins+2)*nshifts)
   ashmesh = np.meshgrid(xgrid, ygrid)
+  ashmesh[0] = ashmesh[0].T
+  ashmesh[1] = ashmesh[1].T
   ashden = np.zeros((nsg, nsg))
 
   for i in range(nshifts):  #x loop
     for j in range(nshifts):  #y loop
       histrange = ( (xmin + i*dsx, xmax + i*dsx -dx), (ymin + j*dsy, ymax + j*dsy - dy) )
       hist, xedge, yedge = np.histogram2d(xvals, yvals, nbins+1, range=histrange, density=False, weights=weights)
-      histdat = np.repeat(hist, nshifts, axis=0)
-      histdat = np.repeat(histdat, nshifts, axis=1)
+      histdat = np.repeat(hist, nshifts, axis=1)
+      histdat = np.repeat(histdat, nshifts, axis=0)
       if (padVals):
         appx1 = np.ones((i,(nbins+1)*nshifts))
         appx2 = np.ones((nshifts - i,(nbins+1)*nshifts))
@@ -67,10 +69,10 @@ def ash2d(xvals, yvals, nbins=20, nshifts=10, weights=None, padVals=False):
         histdat = np.append( appy1, histdat, axis=1)
         histdat = np.append( histdat, appy2, axis=1)
       else:
-        histdat = np.append( np.zeros((i,(nbins+1)*nshifts)), histdat, axis=0)
-        histdat = np.append( histdat, np.zeros((nshifts-i, (nbins+1)*nshifts)), axis=0)
-        histdat = np.append( np.zeros(((nbins+2)*nshifts,j)), histdat, axis=1)
-        histdat = np.append( histdat, np.zeros(( (nbins+2)*nshifts,nshifts-j)), axis=1)
+        histdat = np.append( np.zeros(((nbins+1)*nshifts,j)), histdat, axis=1)
+        histdat = np.append( histdat, np.zeros(( (nbins+1)*nshifts,nshifts-j)), axis=1)
+        histdat = np.append( np.zeros((i,(nbins+2)*nshifts)), histdat, axis=0)
+        histdat = np.append( histdat, np.zeros((nshifts-i, (nbins+2)*nshifts)), axis=0)
       ashden += histdat/np.sum(histdat*(xmax - xmin - dx)*(ymax - ymin - dy)/((nbins+1)*nshifts)**2)
 
   ashden /= nshifts
